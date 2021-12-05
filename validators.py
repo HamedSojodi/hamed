b275e0b967e397b02bb60174539cd5dfd479f5a3
hamedsjd999 <77836662+hamedsjd999@users.noreply.github.com>
2021-12-05 16:34:55 +0330

    Checking username and Email with Regex

--- validators.py
+++ validators.py
@@ -1,4 +1,6 @@
 import re
+
+
 def check_user_name(user_name):
     pattern=r"[a-zA-Z_]{5,14}"
     res = re.fullmatch(pattern, user_name)
@@ -8,4 +10,12 @@ def check_user_name(user_name):
         print("Invalid user name.. ")
 
 
+def ceck_email(email):
+    pat=r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b'
+    res = re.fullmatch(pat, email)
+    if res:
+        print("Yes , It is okay.. ")
+    else:
+        print("Invalid Email.. ")
+a=ceck_email('hamedsojodi1374@gmail.com')
 
