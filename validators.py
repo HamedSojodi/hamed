a92011882eae20ee211de43e9b6885ad896c7da3
hamedsjd999 <77836662+hamedsjd999@users.noreply.github.com>
2021-12-05 18:40:12 +0330

    Checking username and Email and phone with Regex

--- validators.py
+++ validators.py
@@ -10,12 +10,22 @@ def check_user_name(user_name):
         print("Invalid user name.. ")
 
 
-def ceck_email(email):
+def check_email(email):
     pat=r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b'
     res = re.fullmatch(pat, email)
     if res:
         print("Yes , It is okay.. ")
     else:
         print("Invalid Email.. ")
-a=ceck_email('hamedsojodi1374@gmail.com')
+
+
+def check_phone(phone):
+    pattern=r'(09|\+98)(\d|3[0-3-5-6-7-8-9]|01)\d{8}'
+    res =re.fullmatch(pattern, phone)
+    if res:
+        print("Yes , It is okay.. ")
+    else:
+        print("Invalid phone.. ")
+
+
 
