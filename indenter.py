68b839cdb715527d192b92b24d767f44682a0d98
hamedsjd999 <77836662+hamedsjd999@users.noreply.github.com>
2021-12-07 19:26:40 +0330

    Contex maneger

---
+++ indenter.py
@@ -0,0 +1,22 @@
+class Indenter:
+    def __init__(self):
+        self.level = 0
+
+    def __enter__(self):
+        self.level += 1
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.level -=1
+
+    def print(self, text):
+        print('    ' * self.level + text)
+
+
+with Indenter() as indent:
+    indent.print("hi!")
+    with indent:
+        indent.print("talk is cheap")
+        with indent:
+            indent.print("show me code")
+    indent.print("Torvalds")
