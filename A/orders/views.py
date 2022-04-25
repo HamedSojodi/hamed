b8b385ec5a2c2efa9b58cce17d5b71ad67cd016b
kian rayaneh <hamedsojodi1374@gmail.com>
2022-04-25 15:56:28 +0430

    build a cart module to add and sava orders

--- A/orders/views.py
+++ A/orders/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from home.models import Product
+from .cart import Cart
+from .forms import CartAddForm
 
 
 class CartView(View):
@@ -8,5 +11,10 @@ class CartView(View):
 
 
 class CartAddView(View):
-    def post(self, request, prudoct_id):
-        pass
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
