e9dfd9f0efad79adf91bb200842576261afef2d1
kian rayaneh <hamedsojodi1374@gmail.com>
2022-04-30 16:31:22 +0430

    connections to zarinpal

--- A/orders/views.py
+++ A/orders/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from home.models import Product
 from .cart import Cart
 from .forms import CartAddForm
 from orders.models import Order, OrderItem
+import requests
+import json
 
 
 class CartView(View):
@@ -46,3 +49,82 @@ class CreateOrderView(LoginRequiredMixin, View):
                                      quantity=item['quantity'])
         cart.clear()
         return redirect('orders:order_detail', order.id)
+
+
+# zarinpall config
+
+MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
+ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
+ZP_API_STARTPAY = "https://www.zarinpal.com/pg/StartPay/{authority}"
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+email = 'hamedsojodi1374@mail.com.com'  # Optional
+mobile = '09364067949'  # Optional
+# Important: need to edit for realy server.
+CallbackURL = 'http://127.0.0.1:8000/orders/verify/'
+
+
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        request.session['order_pay'] = {
+            'order_id': order.id,
+        }
+        req_data = {
+            "merchant_id": MERCHANT,
+            "amount": order.get_total_price(),
+            "callback_url": CallbackURL,
+            "description": description,
+            "metadata": {"mobile": mobile, "email": email}
+        }
+        req_header = {"accept": "application/json",
+                      "content-type": "application/json'"}
+        req = requests.post(url=ZP_API_REQUEST, data=json.dumps(
+            req_data), headers=req_header)
+        authority = req.json()['data']['authority']
+        if len(req.json()['errors']) == 0:
+            return redirect(ZP_API_STARTPAY.format(authority=authority))
+        else:
+            e_code = req.json()['errors']['code']
+            e_message = req.json()['errors']['message']
+            # In real projects it should be edited to the page
+            return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+
+
+class OrderVerifyView(LoginRequiredMixin, View):
+    def get(self, request):
+        order_id =request.session['order_pya']['order_id']
+        order=Order.objects.get(id = int(order_id))
+        t_status = request.GET.get('Status')
+        t_authority = request.GET['Authority']
+        if request.GET.get('Status') == 'OK':
+            req_header = {"accept": "application/json",
+                          "content-type": "application/json'"}
+            req_data = {
+                "merchant_id": MERCHANT,
+                "amount": order.get_total_price(),
+                "authority": t_authority
+            }
+            req = requests.post(url=ZP_API_VERIFY, data=json.dumps(req_data), headers=req_header)
+            if len(req.json()['errors']) == 0:
+                t_status = req.json()['data']['code']
+                if t_status == 100:
+                    order.paid =True
+                    order.save()
+                    return HttpResponse('Transaction success.\nRefID: ' + str(
+                        req.json()['data']['ref_id']
+                    ))
+                elif t_status == 101:
+                    return HttpResponse('Transaction submitted : ' + str(
+                        req.json()['data']['message']
+                    ))
+                else:
+                    return HttpResponse('Transaction failed.\nStatus: ' + str(
+                        req.json()['data']['message']
+                    ))
+            else:
+                e_code = req.json()['errors']['code']
+                e_message = req.json()['errors']['message']
+                return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+        else:
+            return HttpResponse('Transaction failed or canceled by user')
